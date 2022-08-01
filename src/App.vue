69276d61cb7e5c8bb2a1a9136cb5c62190117c6f
Alex Javier Brito Muñoz <74514983+Alexprograming@users.noreply.github.com>
2022-08-01 15:38:50 -0500

    commit1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import InputTag from "./components/InputTag.vue";
+</script>
+
+<template>
+Desarrollo con Vue.js
+<InputTag></InputTag>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/InputTag.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    data(){
+        return {
+        //2 propiedades
+            currentValue:"",
+            tags:[],
+        }
+    },
+    methods:{ //Creamos un evento
+        handleKeyDown(e){
+            if(e.key = 'Enter' && this.currentValue != ``){
+                this.tags.push(this.currentValue);
+            }
+        },
+        handleSubmit(){
+            if(this.currentValue != ""){
+                this.tags.push(this.currentValue);
+                //Borrar valor actual
+                this.currentValue = "";
+        }
+        },
+        deleteTag(tag){
+            this.tags = this.tags.filter((item) => item != tag);
+        },
+    },
+};
+</script>
+<template>
+    <div class="inputTag">
+        <div class="tags">
+            <div class="tag" v-for="(tag , index) in tags" :key="index">
+            <!--El valor de tag-->
+            {{ tag }}<button @click="deleteTag(tag)">X</button>
+            </div>
+        </div>
+        <!-- Prevent omitir el comportamiento de mi formulario -->
+        <form @submit.prevent="handleSubmit">
+        <input type="text" v-model="currentValue" @keydown="handleKeyDown"/>
+        </form>
+    </div>
+</template>
+<style scoped></style>
